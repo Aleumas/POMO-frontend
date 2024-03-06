cc68221aafd959c3c5d62237df9a4fad754e1bf0
Samuel Asamoah <aleumas25@gmail.com>
2024-03-05 23:33:47 -0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { io, Socket } from "socket.io-client";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [seconds, updateSeconds] = useState<number>(0);
+  const [startTime, setStartTime] = useState<number>(0);
+  const [socket, setSocket] = useState<Socket>();
+
+  useEffect(() => {
+    const socket = io(process.env.SOCKET_SERVER_URL || "http://localhost:3000");
+    setSocket(socket);
+
+    socket.on("update-timer", (time: number) => {
+      updateSeconds(time);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>Pomodoro: {seconds}</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          socket?.emit("start", startTime);
+        }}
+      >
+        <input
+          onChange={(e) => setStartTime(parseInt(e.target.value))}
+          type="number"
+        />
+        <button type="submit">Start Timer</button>
+      </form>
+    </>
+  );
+}
