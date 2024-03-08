9f70f87747981e8ba4cf8acc9de1f932cd1db848
Samuel Asamoah <aleumas25@gmail.com>
2024-03-07 21:38:13 -0500

    add auth0 authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,25 @@
 "use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
-import { io, Socket } from "socket.io-client";
-import { useEffect, useState } from "react";
-
-export default function Page() {
-  const [seconds, updateSeconds] = useState<number>(0);
-  const [startTime, setStartTime] = useState<number>(0);
-  const [socket, setSocket] = useState<Socket>();
-
-  useEffect(() => {
-    const socket = io(process.env.SOCKET_SERVER_URL || "http://localhost:3000");
-    setSocket(socket);
-
-    socket.emit("create-room");
-
-    socket.on("update-timer", (time: number) => {
-      updateSeconds(time);
-    });
-
-    socket.on("room-created", (room: string) => {
-      console.log("room created: ", room);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+export default () => {
+  const { user, error, isLoading } = useUser();
 
   return (
     <>
-      <h1>Pomodoro: {seconds}</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          socket?.emit("start-timer", startTime);
-        }}
-      >
-        <input
-          onChange={(e) => setStartTime(parseInt(e.target.value))}
-          type="number"
-        />
-        <button type="submit">Start Timer</button>
-      </form>
+      {isLoading && <div>Loading...</div>}
+
+      {error && (
+        <div>
+          {error.name}: {error.message}
+        </div>
+      )}
+
+      {user && (
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.sub}</p>
+        </div>
+      )}
     </>
   );
-}
+};
