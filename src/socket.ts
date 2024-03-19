d80b8a7c0f305fe097b32718800e100632620846
Samuel Asamoah <aleumas25@gmail.com>
2024-03-19 18:45:01 -0400

    reconnect to socket on failure

--- src/socket.ts
+++ src/socket.ts
@@ -4,6 +4,7 @@ const URL =
   process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
 
 export const socket = io(URL, {
-  autoConnect: false,
-  reconnectionAttempts: 5,
+  reconnection: true,
+  reconnectionDelay: 500,
+  reconnectionAttempts: 10,
 });
