db1050ac0c2a3e571b478973e40aa3c767492b3d
Samuel Asamoah <aleumas25@gmail.com>
2024-03-18 21:36:34 -0400

    block invalid room urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,21 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
-export default withMiddlewareAuthRequired();
+export async function middleware(request, event) {
+  const pathname = request.nextUrl.pathname;
+  if (pathname.startsWith("/room/")) {
+    const parts = pathname.split("/");
+
+    const id = parts[2];
+
+    try {
+      const response = await fetch(`http://localhost:3000/rooms/${id}`);
+      if (!response.ok) {
+        throw new Error("Room not found");
+      }
+    } catch (error) {
+      return new Response(error.message, { status: 404 });
+    }
+  }
+
+  return withMiddlewareAuthRequired()(request, event);
+}
